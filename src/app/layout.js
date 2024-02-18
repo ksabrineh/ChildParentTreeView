c22dc3c12874245fd184a57094b1e8310c94676b
Sabrineh <ksabrineh@gmail.com>
2024-02-18 23:34:04 +0330

    change name

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "treeview by sabrineh",
+  title: "Treeview by Sabrineh",
   description: "Generated for child parent treeview",
 };
 
