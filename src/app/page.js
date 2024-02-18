a8ea5f5374559c81febb3d23db49aa2100afabf1
Sabrineh <ksabrineh@gmail.com>
2024-02-18 23:32:48 +0330

    sabrineh : remove comment codes 2024/02/18 11:32

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,9 @@
 "use client";
-import Image from "next/image";
 import TreeViewComponenet from "../../components/TreeView";
-import { useCallback } from "react";
-import Particles from "@tsparticles/react";
-import { loadFull } from "tsparticles";
 export default function Home() {
-  // const particlesInit = useCallback(async (engine) => {
-  //   console.log(engine);
-  //   await loadFull(engine);
-  // }, []);
-
-  // const particlesLoaded = useCallback(async (container) => {
-  //   await console.log(container);
-  // }, []);
   return (
     <div className="flex h-screen overflow-hidden w-[900px] justify-center items-center backdrop-blur-sm">
       <TreeViewComponenet />
-      {/* <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          background: {
-            color: {
-              value: "#ffff",
-            },
-          },
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 6,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
-      /> */}
     </div>
   );
 }
